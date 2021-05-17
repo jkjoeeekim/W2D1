8e4077797049aac9bb567951888ac6adc5a53b21
Joseph Kim <jkjoeeekim@live.com>
2021-05-17 10:59:50 -0700

    Finish dog_project

--- dog_project/lib/dog.rb
+++ dog_project/lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    attr_reader :name, :breed, :favorite_foods
+    attr_accessor :age
 
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def bark
+        age > 3 ? @bark.upcase : @bark.downcase
+    end
+
+    def favorite_food?(food)
+        favorite_foods.any? { |fav_food| fav_food.downcase == food.downcase }
+    end
 end
